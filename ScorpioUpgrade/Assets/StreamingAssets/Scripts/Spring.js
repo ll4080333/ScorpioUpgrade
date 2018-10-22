d90080c7924ae4db9347039c87b2f9ca571de697
SpringDong <springu3d@yeah.net>
2018-10-22 23:13:04 +0800

    udpate

--- ScorpioUpgrade/Assets/StreamingAssets/Scripts/Spring.js
+++ ScorpioUpgrade/Assets/StreamingAssets/Scripts/Spring.js
@@ -2,36 +2,7 @@
 
 Spring = 
 {
-    // table : null;
-    // // prototype : "config.js";
-    // prototype = 
-    // {
-    //     author : "dongwei"
-    // }
+    
 }
 
-// Spring.Name = "Springdong";
-
-// Spring.
-
-// Spring.Init = function()
-// {
-
-// }
-
-// Spring.Release = function()
-// {
-
-// }
-
-// Spring.Find("Child");
-
-// for( var i = 0 ; i < 10 ; i++ )
-// {
-//     print("number + " + i);
-// }
-
-// print("Spring load successfully.");
-print(Spring.name);
-// print(Spring.prototype.author);
-// print(Spring.Init.Name);
+ print("Spring load successfully.");
